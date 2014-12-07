1d3c68992e6722af7f7f38eebd0d27e83ce0703c
Klemen Košir <klemen.kosir@kream.io>
2014-12-07 14:52:39 +0100

    ClearDB support.

--- src/main/java/io/kream/rss/HelloBean.java
+++ src/main/java/io/kream/rss/HelloBean.java
@@ -1,13 +1,63 @@
 package io.kream.rss;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 @ManagedBean(name = "helloBean")
 @SessionScoped
 public class HelloBean {
 	private String email;
 	private String password;
+	
+//	@Resource(name = "jdbc/database")
+	DataSource ds;
+	
+	public HelloBean()
+	{
+		try
+		{
+			Context ctx = new InitialContext();
+			ds = (DataSource)ctx.lookup("java:comp/env/jdbc/database");
+		}
+		catch(NamingException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public String validate()
+	{
+		Connection conn = null;
+		PreparedStatement ps = null;
+		
+		try
+		{
+			String sql = "SELECT id, email FROM Users where email = ?";
+			conn = ds.getConnection();
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, email);
+			
+			ResultSet rs = ps.executeQuery();
+			rs.first();
+			
+			return rs.getString("email");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return "FAIL";
+	}
 	
 	public String getEmail()
 	{
